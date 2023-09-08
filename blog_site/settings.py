988f9b091a20c1e4a34e4474409b0adb44884629
Tasmia-Mitu <tasmiamitu5@gmail.com>
2023-09-08 09:22:20 +0600

    update

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blog_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,14 +117,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    BASE_DIR/'static',
+    BASE_DIR / 'static',
 ]
 
 
--- blog_site/urls.py
+++ blog_site/urls.py
@@ -25,8 +25,13 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
-    path('contact', views.contact, name = 'contact'),
 
     path('account/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+
+] 
+
+# Serve static and media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
